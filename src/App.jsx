81091ecaa2b64ced4c02bb104b0527d102df5a18
saraabbih1 <abbihsara30@gmail.com>
2026-03-30 17:09:07 +0100

    feat:addtaskfunction

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
-import "./App.css";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  const addTask = (title) => {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div>
       <h1>To-Do App</h1>
+      <TaskInput addTask={addTask} />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
