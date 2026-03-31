389370f82e133bcaab48c296e02a072ec01fd08c
saraabbih1 <abbihsara30@gmail.com>
2026-03-31 16:05:25 +0100

    feat:filter

---
+++ src/components/Filter.jsx
@@ -0,0 +1,11 @@
+function Filter({ setFilter }) {
+  return (
+    <div>
+      <button onClick={() => setFilter("all")}>All</button>
+      <button onClick={() => setFilter("completed")}>Completed</button>
+      <button onClick={() => setFilter("pending")}>Pending</button>
+    </div>
+  );
+}
+
+export default Filter;
