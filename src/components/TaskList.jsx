54cb8faa5930353b5e0da4eeac0735484509cc92
saraabbih1 <abbihsara30@gmail.com>
2026-04-01 17:10:34 +0100

    update:tasklist

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,36 +1,62 @@
 function TaskList({ tasks, deleteTask, toggleTask }) {
+  if (tasks.length === 0)
+    return <p style={{ textAlign: "center" }}>No tasks</p>;
+
+const container = {
+  display: "flex",
+  flexDirection: "column", // ✅ باش يجيو تحت بعضهم
+  gap: "10px",
+  alignItems: "center",
+};
+
+const taskCard = {
+  width: "100%",
+  maxWidth: "500px",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "15px",
+  borderRadius: "12px",
+  backgroundColor: "#2B124C", // ✅ غامق
+  boxShadow: "0 4px 10px rgba(0,0,0,0.3)",
+};
+
+const deleteBtn = {
+  backgroundColor: "#854F6C",
+  color: "#FBE4D8",
+  border: "none",
+  padding: "6px 12px",
+  borderRadius: "8px",
+  cursor: "pointer",
+};
+
   return (
-   <div
-  
-  style={{
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    background: "#fff",
-    margin: "10px 0",
-    padding: "10px",
-    borderRadius: "10px",
-    boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
-  }}
->
+    <div style={container}>
       {tasks.map((task) => (
-        <div key={task.id}>
-          <input
-            type="checkbox"
-            checked={task.completed}
-            onChange={() => toggleTask(task.id)}
-          />
+        <div key={task.id} style={taskCard}>
+          
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTask(task.id)}
+            />
 
-          <span
-            style={{
-              textDecoration: task.completed ? "line-through" : "none",
-              margin: "10px",
-            }}
-          >
-            {task.title}
-          </span>
+            <span
+              style={{
+                color: "#FBE4D8", // ✅ text واضح
+                textDecoration: task.completed ? "line-through" : "none",
+                fontWeight: "500",
+              }}
+            >
+              {task.title}
+            </span>
+          </div>
 
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
+          <button style={deleteBtn} onClick={() => deleteTask(task.id)}>
+            Delete
+          </button>
+
         </div>
       ))}
     </div>
